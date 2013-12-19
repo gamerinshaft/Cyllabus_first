b23cfb0bcf40c864ab7cb01b66fc8802d9730382
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-20 08:42:05 +0900

    migration database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219233655) do
+ActiveRecord::Schema.define(version: 20131219233902) do
+
+  create_table "blank_questions", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "companies", force: true do |t|
     t.string   "company_name"

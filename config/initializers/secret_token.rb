66b4e381963e529cdfc12d6d8358944c185144b9
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-19 18:22:05 +0900

    initial commit Rails 4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyllabus::Application.config.secret_key_base = '2c28db33ab44abf4e9582009825db3f9386915981592b2be7fc7f92c52d6f8204895e5729c55e1e58a31a169c7cdfcf151f943c8aa85c3020a6dfbd0b8a550ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cyllabus::Application.config.session_store :cookie_store, key: '_cyllabus_session'

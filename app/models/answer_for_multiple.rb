e4c2bc39d8ea830b890021fb6999b3b918ca7de8
Ken Ohata <ohata@daraf.co.jp>
2013-12-21 00:32:45 +0900

    add validation

--- app/models/answer_for_multiple.rb
+++ app/models/answer_for_multiple.rb
@@ -1,3 +1,5 @@
 class AnswerForMultiple < ActiveRecord::Base
 	belongs_to :multiple_choice_question
+
+  validates :multiple_choice_question, presence: true
 end
--- app/models/multiple_choice_question.rb
+++ app/models/multiple_choice_question.rb
@@ -1,4 +1,6 @@
 class MultipleChoiceQuestion < ActiveRecord::Base
 	belongs_to :company
 	has_many :answer_for_multiples
+
+  validates :company, presence: true
 end

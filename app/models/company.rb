b6844812865c86da2e9cf639c8bace7382a7bc1e
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-20 16:28:59 +0900

    setting relation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ActiveRecord::Base
 	mount_uploader :logo, LogoUploader
 	mount_uploader :cover, CoverUploader
+	has_many :multiple_choice_questions
 	geocoded_by :address
 	after_validation :geocode
 end
